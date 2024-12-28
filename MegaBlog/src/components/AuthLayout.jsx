f48040038f252a61b7bd8d5f51516692f03a0c6a
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-12-28 12:04:21 +0500

    Update AuthLayout.jsx

--- MegaBlog/src/components/AuthLayout.jsx
+++ MegaBlog/src/components/AuthLayout.jsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
+// ------------ Mechanism of protect the rout and pages -----------------------------
 export default function Protected({children, authentication = true}) {
-
+    // Navigate work like Redirect the user in this function
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
-    const authStatus = useSelector((state) => state.auth.status)
+    // Check user login or not ask in store
+    const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
         
         //TODO: make it more easy to understand
 
-        // if (authStatus ===true){
+        // if (authStatus === true){
         //     navigate("/")
         // } else if (authStatus === false) {
         //     navigate("/login")
@@ -20,15 +22,19 @@ export default function Protected({children, authentication = true}) {
         
         //let authValue = authStatus === true ? true : false
 
+        // true && false !=== true The result is true = true
         if(authentication && authStatus !== authentication) {
             navigate('/login')
+        // ( !true = false ) && true !=== true The result is false = false
         }else if(!authentication && authStatus !== authentication){
             navigate('/')
         }
+
         setLoader(false)
+
     }, [authStatus, navigate, authentication])
 
-    return loader ? <h1>Loading...</h1> : <>{children}</>
+    return loader ? <h1>Loading...</h1> : <>{ children }</>
 }
 
 
