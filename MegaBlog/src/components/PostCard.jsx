5fac37c35a728920323e0962bdc19337cf4c21e2
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-12-28 12:06:54 +0500

    Update PostCard.jsx

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import appwriteService from '../appwrite/conf'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import service from "../appwrite/conf";
 
-function PostCard({$id, title, featuredImage}) {
+export default function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl'/>
-            </div>
-            <h2 className='text-xl font-bold'>{title}</h2>
+      <div className="w-full text-black rounded-xl p-4 shadow-lg">
+        <div className="w-full justify-center mb-4">
+          <img
+            className="w-48 h-48"
+            src={service.filePreview(featuredImage)}
+            alt={title}
+          />
         </div>
+        <h2 className="text-xl font-bold text-black">{title}</h2>
+      </div>
     </Link>
-  )
+  );
 }
-
-export default PostCard
