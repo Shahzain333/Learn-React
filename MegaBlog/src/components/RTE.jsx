d4185fcab8f461b5f507d1231969b849f2ca8bb6
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-12-28 12:07:26 +0500

    Update RTE.jsx

--- MegaBlog/src/components/RTE.jsx
+++ MegaBlog/src/components/RTE.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import {Controller } from 'react-hook-form';
+import { Controller } from "react-hook-form";
+import conf from "../config/config";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
-  return (
+  // the control in responsible to transfer the state of the editor to the form in another component (parent component).
+  // or the control will transfer the control to another component from where the this component is called.
 
-    <div className="w-full">
-      
-      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+  return (
+    
+    <div>
+      {label && (
+        <label className="font-medium inline-block mb-1 pl-1 text-left">
+          {label}
+        </label>
+      )}
 
       <Controller
-        name={name || "content"}
+        name={name || "editor"}
         control={control}
         render={({ field: { onChange } }) => (
-          
           <Editor
+            // apiKey = {conf.rteAPIKey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
               height: 500,
               menubar: true,
-              plugins: ["image","advlist","autolink","lists","link","image","charmap","preview","anchor",
-                "searchreplace","visualblocks","code","fullscreen","insertdatetime","media","table","code",
-                "help","wordcount","anchor",
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
               ],
               toolbar:
                 "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
@@ -31,14 +56,15 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
             }}
 
             onEditorChange={onChange}
-
-          />
           
+          />
+        
         )}
-
+      
       />
-
+    
     </div>
 
   );
+
 }
