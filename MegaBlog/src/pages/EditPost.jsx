23b27cc7f2686e49b7b4c13f2791af84eda957e6
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-12-28 13:14:20 +0500

    Update EditPost.jsx

--- MegaBlog/src/pages/EditPost.jsx
+++ MegaBlog/src/pages/EditPost.jsx
@@ -1,37 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { Container, PostForm } from '../components/Index'
-import appwriteService from '../appwrite/conf'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Container, PostForm } from "../components";
+import service from "../appwrite/conf";
+import { useNavigate, useParams } from "react-router-dom";
 
+export default function EditPost(props) {
+  const [post, setPost] = React.useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-function EditPost() {
-    
-    const [post, setPosts] = useState(null)
-
-    const {slug} = useParams()
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        
-        if(slug) {
-            appwriteService.getPost(slug).then((post) => {
-                if(post) {
-                    setPosts(post)
-                }
-            })
-        } else {
-            navigate('/')
+  useEffect(() => {
+    if (slug) {
+      service.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
         }
-
-    }, [slug, navigate])
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
 
   return post ? (
-    <div className='py-8'>
-        <Container>
-            <PostForm post={post}/>
-        </Container>
+    <div className="py-8 ">
+      <Container>
+        <PostForm post={post} />
+      </Container>
     </div>
-  ) : null
+  ) : null;
 }
-
-export default EditPost
